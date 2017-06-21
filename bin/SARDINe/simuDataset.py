4cf6b1ef7c629e3e5fcb4ab8ec009d427f417897
fescudie <escudie.frederic@wanadoo.fr>
2017-06-21 22:04:09 +0200

    Add quality penalty and sample id.

--- bin/SARDINe/simuDataset.py
+++ bin/SARDINe/simuDataset.py
@@ -19,7 +19,7 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2017 IUCT'
 __license__ = 'GNU General Public License'
-__version__ = '1.0.0'
+__version__ = '1.2.0'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -28,7 +28,6 @@ import sys
 import logging
 import argparse
 import subprocess
-from subprocess import Popen, PIPE
 
 
 ########################################################################
@@ -37,42 +36,76 @@ from subprocess import Popen, PIPE
 #
 ########################################################################
 if __name__ == "__main__":
-	# Manage parameters
-	parser = argparse.ArgumentParser( description='**************************************************************.' )
-	parser.add_argument( '-n', '--samples-names', nargs="+", default=["withVar-splA", "withVar-splB", "withVar-splC", "withVar-splD", "withVar-splE", "withVar-splF", "withVar-splG", "withVar-splH", "withVar-splI", "withVar-splJ"], help="The *****************************************. [Default: %(default)s]" )
-	parser.add_argument( '-d', '--min-distance', type=int, default=2, help="The *****************************************. [Default: %(default)s]" )
-	parser.add_argument( '-v', '--version', action='version', version=__version__ )
-	group_input = parser.add_argument_group( 'Inputs' ) # Inputs
-	group_input.add_argument( '-m', '--input-manifest', default="INCa_V1_A_TC0070336.txt", help='*********************************** (format: *****************). [Default: %(default)s]' )
-	group_input.add_argument( '-p', '--input-profile', default="variants_profiles.tsv", help='Path to the variants profile file (format: TSV). [Default: %(default)s]' )
-	group_input.add_argument( '-r', '--input-runs', nargs="+", default=["/Anapath/Illumina\ Run\ datas/Routine/INCa-V1_160928_M70265_0018_000000000-ANC7J", "/Anapath/Illumina\ Run\ datas/Routine/INCa-V1_161026_M70265_0023_000000000-ANBWJ", "/Anapath/Illumina\ Run\ datas/Routine/INCa-V1_161116_M70265_0027_000000000-ANC6L", "/Anapath/Illumina\ Run\ datas/Routine/INCa-V1_161207_M70265_0031_000000000-AU8FB", "/Anapath/Illumina\ Run\ datas/Routine/INCa-V1_170118_M70265_0038_000000000-AYAKV", "/Anapath/Illumina\ Run\ datas/Routine/INCa-V1_170113_M02726_0179_000000000-AY3E6"], help='Path to the variants profile file (format: TSV). [Default: %(default)s]' )
-	group_output = parser.add_argument_group( 'Outputs' ) # Outputs
-	group_output.add_argument( '-o', '--output-folder', default=os.getcwd(), help='***************************************. [Default: %(default)s]' )
-	args = parser.parse_args()
+    # Manage parameters
+    parser = argparse.ArgumentParser( description='Creates datasets (several samples and runs) of simulated reads corresponding to Illumina sequencing of amplicon double strand experiment.***************' )
+    parser.add_argument( '-n', '--samples-names', nargs="+", default=["splA", "splB", "splC", "splD", "splE", "splF", "splG", "splH", "splI", "splJ"], help="The names of simulated samples. [Default: %(default)s]" )
+    parser.add_argument( '-s', '--id-start', type=int, default=1, help="********************************************. [Default: %(default)s]" )
+    parser.add_argument( '-d', '--min-distance', type=int, default=2, help="The minimum distance between two variants. [Default: %(default)s]" )
+    parser.add_argument( '-q', '--qual-penalty', type=int, default=0, help="The penalty applied to reduce the quality of the sequences produced. With 2, the quality of each base is decrease of 2 compared to the model. [Default: %(default)s]" )
+    parser.add_argument( '-v', '--version', action='version', version=__version__ )
+    group_input = parser.add_argument_group( 'Inputs' ) # Inputs
+    group_input.add_argument( '-m', '--input-manifest', required=True, help='Path to the definition of the amplicons (format: Illumina manifest).' )
+    group_input.add_argument( '-p', '--input-profile', required=True, help='Path to the variants profile file (format: TSV).' )
+    group_input.add_argument( '-g', '--input-genome', required=True, help='Path to the genome sequences file (format: fasta).' )
+    group_input.add_argument( '-r', '--input-runs', required=True, nargs="+", help='Path to the variants profile file (format: TSV).' )
+    group_output = parser.add_argument_group( 'Outputs' ) # Outputs
+    group_output.add_argument( '-o', '--output-folder', default=os.getcwd(), help='The output folder for the dataset. [Default: %(default)s]' )
+    args = parser.parse_args()
 
-	# Logger
-	logging.basicConfig(
-		level=logging.DEBUG,
-		format='%(asctime)s -- [%(name)s][pid:%(process)d][%(levelname)s] -- %(message)s' )
-	logger = logging.getLogger( os.path.basename(__file__) )
+    # Logger initialisation
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s -- [%(name)s][pid:%(process)d][%(levelname)s] -- %(message)s' )
+    logger = logging.getLogger( os.path.basename(__file__) )
+    logging.info( " ".join(sys.argv) )
 
-	for current_spl in args.samples_names:
-		logging.info(current_spl)
-		cmd = "python3 /save/fescudie/scripts/SARDINe/simuAmpliDS.py --sample-name " + current_spl + " --min-distance " + str(args.min_distance) + " --input-manifests " + args.input_manifest + " --input-genome /work/fescudie/bank/Homo_sapiens/DNA/GRCh19_Broad/ucsc.hg19.fasta --input-profile " + args.input_profile + " --output-variants " + os.path.join(args.output_folder, current_spl + ".vcf") + " --output-folder " + args.output_folder
-		subprocess.check_output( cmd, shell=True )
-		for sample_fasta in os.listdir( args.output_folder ):
-			if sample_fasta.startswith(current_spl) and sample_fasta.endswith(".fasta"):
-				logging.info("\t" + sample_fasta)
-				sample_name = os.path.basename(sample_fasta).split(".")[0]
-				read = "R1"
-				if sample_name.endswith("R2"):
-					read = "R2"
-				for idx_model, model in enumerate(args.input_runs):
-					model_files_regexp = model + "/Data/Intensities/BaseCalls/*_" + read + "_00*.fastq.gz"
-					out_fastq_path = os.path.join( args.output_folder, "run-" + str(idx_model) + "-" + sample_name + ".fastq" )
-					out_trace_path = os.path.join( args.output_folder, "run-" + str(idx_model) + "-" + sample_name + "_error.tsv" )
-					cmd = "python3 /save/fescudie/scripts/SARDINe/applyQualityProfile.py --input-models " + model_files_regexp + " --input-sequences " + os.path.join(args.output_folder, sample_fasta) + " --output-sequences " + out_fastq_path + " --output-trace " + out_trace_path
-					subprocess.check_output( cmd, shell=True )
-					cmd = "gzip " + out_fastq_path
-					subprocess.check_output( cmd, shell=True )
-				os.remove( os.path.join(args.output_folder, sample_fasta) )
+    # Dataset creation
+    max_lib_id = args.id_start
+    lib_by_spl = dict()
+    for current_spl in args.samples_names:
+        # Simulate variants and perfect reads
+        logging.info(current_spl)
+        cmd = "simuAmpliDS.py" + \
+              " --is-double-strand" + \
+              " --sample-name " + current_spl + \
+              " --min-distance " + str(args.min_distance) + \
+              " --input-manifests " + args.input_manifest + \
+              " --input-genome " + args.input_genome + \
+              " --input-profile " + args.input_profile + \
+              " --output-variants " + os.path.join(args.output_folder, current_spl + ".vcf") + \
+              " --output-folder " + args.output_folder
+        logging.info( "\t[SPL " + current_spl + "] " + cmd )
+        subprocess.check_output( cmd, shell=True )
+        # Apply sequencer error model on reads
+        for sample_fasta in os.listdir( args.output_folder ):
+            if sample_fasta.startswith(current_spl) and sample_fasta.endswith(".fasta"):
+                logging.info("\t" + sample_fasta)
+                filename_wot_ext = os.path.basename(sample_fasta).split(".")[0]
+                sample_name = filename_wot_ext[:-5]
+                lib = filename_wot_ext[-4:-3]
+                read = "R2" if filename_wot_ext.endswith("R2") else "R1"
+                curr_lib_id = None
+                if filename_wot_ext[:-3] in lib_by_spl:
+                    curr_lib_id = lib_by_spl[filename_wot_ext[:-3]]
+                else:
+                    curr_lib_id = str(max_lib_id)
+                    lib_by_spl[filename_wot_ext[:-3]] = curr_lib_id
+                    max_lib_id += 1
+                for idx_model, model in enumerate(args.input_runs):
+                    run_folder = os.path.join( args.output_folder, "run_" + str(idx_model) )
+                    if not os.path.exists(run_folder):
+                        os.makedirs(run_folder)
+                    model_files_regexp = model + "/Data/Intensities/BaseCalls/*_" + read + "_00*.fastq.gz"
+                    out_fastq_path = os.path.join( run_folder, sample_name + "_S" + curr_lib_id + "_L001_" + read + "_001.fastq" )
+                    out_trace_path = os.path.join( run_folder, sample_name + "_S" + curr_lib_id + "_L001_" + read + "_001_error.tsv" )
+                    cmd = "applyQualityProfile.py" + \
+                          " --qual-penalty " + str(args.qual_penalty) + \
+                          " --input-models " + model_files_regexp + \
+                          " --input-sequences " + os.path.join(args.output_folder, sample_fasta) + \
+                          " --output-sequences " + out_fastq_path + \
+                          " --output-trace " + out_trace_path
+                    logging.info( "\t[SPL " + current_spl + "] " + cmd )
+                    subprocess.check_output( cmd, shell=True )
+                    cmd = "gzip " + out_fastq_path
+                    subprocess.check_output( cmd, shell=True )
+                os.remove( os.path.join(args.output_folder, sample_fasta) )
