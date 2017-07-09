ab773b64101d9e73a72b312db85514bb3f6aee38
fescudie <escudie.frederic@wanadoo.fr>
2017-07-09 19:04:13 +0200

    Add data to test workflows.

---
+++ jflow/workflows/RNASeq/data/RNASeq.sh
@@ -0,0 +1,7 @@
+python3 /save/fescudie/softwares/jflow/bin/jflow_cli.py rnaseq \
+--R1 /work/fescudie/Acinome/genewiz/FastQ/Sample_08H1505-TT/08H1505-TT_ATTACTCG-CAGGACGT_L004_R1_001.fastq.gz \
+--R2 /work/fescudie/Acinome/genewiz/FastQ/Sample_08H1505-TT/08H1505-TT_ATTACTCG-CAGGACGT_L004_R2_001.fastq.gz \
+--genome-dir /work/fescudie/bank/Homo_sapiens/DNA/GRCh38_Ensembl87_std/without_contig/ \
+--genome-annot /work/fescudie/bank/Homo_sapiens/DNA/GRCh38_Ensembl87_std/without_contig/Homo_sapiens.GRCh38.87.chr.gtf \
+--genome-seq /work/fescudie/bank/Homo_sapiens/DNA/GRCh38_Ensembl87_std/without_contig/Homo_sapiens.GRCh38.chr.fa \
+--genome-SNV /work/fescudie/bank/Homo_sapiens/DNA/GRCh38_Ensembl87_std/without_contig/dbsnp_146.hg38.sorted.vcf.gz
