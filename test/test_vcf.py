70c7267eaebe65353b8c084ed243556079348f9a
fescudie <escudie.frederic@wanadoo.fr>
2017-08-14 11:27:50 +0200

    Add unit tests on standardizeSingleAllele for vcf.py.

--- test/test_vcf.py
+++ test/test_vcf.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #
 # Copyright (C) 2017 IUCT-O
 #
@@ -18,12 +19,13 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2017 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __email__ = 'frederic.escudie@iuct-oncopole.fr'
 __status__ = 'prod'
 
 import os
 import sys
+import unittest
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 LIB_DIR = os.path.abspath(os.path.dirname(CURRENT_DIR))
@@ -34,99 +36,137 @@ else: os.environ['PYTHONPATH'] = os.environ['PYTHONPATH'] + os.pathsep + LIB_DIR
 from vcf import VCFRecord, VCFIO
 
 
+########################################################################
+#
+# FUNCTIONS
+#
+########################################################################
+class TestVCFRecord(unittest.TestCase):
+    def testStandardizeSingleAllele(self):
+        # Test substitution one nt
+        substitution = VCFRecord( "artificial_1", 18, None, "TC", ["TA"], 230 )
+        substitution.standardizeSingleAllele()
+        self.assertTrue( substitution.ref == "C" and substitution.alt[0] == "A" and substitution.pos == 19 )
+        # Test substitution multi nt
+        substitution = VCFRecord( "artificial_1", 18, None, "TCtgA", ["TaGCc"], 230 )
+        substitution.standardizeSingleAllele()
+        self.assertTrue( substitution.ref == "CTGA" and substitution.alt[0] == "AGCC" and substitution.pos == 19 )
+        # Test substitution multi nt with possible remove
+        substitution = VCFRecord( "artificial_1", 18, None, "TCtgATT", ["TaGGctT"], 230 )
+        substitution.standardizeSingleAllele()
+        self.assertTrue( substitution.ref == "CTGA" and substitution.alt[0] == "AGGC" and substitution.pos == 19 )
 
-class TestVCFRecord:
-    def getMostUpstream(self):
+        # Test insertion
+        insertion = VCFRecord( "artificial_1", 18, None, "T", ["TA"], 230 )
+        insertion.standardizeSingleAllele()
+        self.assertTrue( insertion.ref == "." and insertion.alt[0] == "A" and insertion.pos == 19 )
+        # Test insertion multi nt and remove upstream and downstream
+        insertion = VCFRecord( "artificial_1", 18, None, "TGAT", ["TCGAGAT"], 230 )
+        insertion.standardizeSingleAllele()
+        self.assertTrue( insertion.ref == "." and insertion.alt[0] == "CGA" and insertion.pos == 19 )
+        # Test insertion multi nt with possible remove and downstream and without complete standardization
+        insertion = VCFRecord( "artificial_1", 18, None, "TCtgATTAGC", ["TaGGctTATGCGC"], 230 )
+        insertion.standardizeSingleAllele()
+        self.assertTrue( insertion.ref == "CTGATTA" and insertion.alt[0] == "AGGCTTATGC" and insertion.pos == 19 )
+
+        # Test deletion
+        insertion = VCFRecord( "artificial_1", 18, None, "TA", ["T"], 230 )
+        insertion.standardizeSingleAllele()
+        self.assertTrue( insertion.ref == "A" and insertion.alt[0] == "." and insertion.pos == 19 )
+        # Test insertion multi nt and remove upstream and downstream
+        insertion = VCFRecord( "artificial_1", 18, None, "TCGAGAT", ["TGAT"], 230 )
+        insertion.standardizeSingleAllele()
+        self.assertTrue( insertion.ref == "CGA" and insertion.alt[0] == "." and insertion.pos == 19 )
+        # Test insertion multi nt with possible remove and downstream and without complete standardization
+        insertion = VCFRecord( "artificial_1", 18, None, "TaGGctTATGCGC", ["TCtgATTAGC"], 230 )
+        insertion.standardizeSingleAllele()
+        self.assertTrue( insertion.ref == "AGGCTTATGC" and insertion.alt[0] == "CTGATTA" and insertion.pos == 19 )
+
+    def testGetMostUpstream(self):
         ref = "nnNNATGCCAaTgATGTTtTaAGCCGAGCCGAT" # length: 33
-        error_msg = "Error in method getMostUpstream() for class VCFRecord."
         # Test fix deletion
         deletion = VCFRecord( "artificial_1", 18, None, "TtTaAGC", ["T"], 230 )
         upstream = deletion.getMostUpstream( ref )
-        assert (upstream.pos == 19 and upstream.ref == "TTAAGC" and upstream.alt[0] == "."), error_msg
+        self.assertTrue( upstream.pos == 19 and upstream.ref == "TTAAGC" and upstream.alt[0] == "." )
         # Test homopolymer deletion
         deletion = VCFRecord( "artificial_1", 18, None, "TTT", ["T"], 230 )
         upstream = deletion.getMostUpstream( ref )
-        assert (upstream.pos == 17 and upstream.ref == "TT" and upstream.alt[0] == "."), error_msg
+        self.assertTrue( upstream.pos == 17 and upstream.ref == "TT" and upstream.alt[0] == "." )
         # Test complex deletion
         deletion = VCFRecord( "artificial_1", 25, None, "CGAgCC", ["C"], 230 )
         upstream = deletion.getMostUpstream( ref )
-        assert (upstream.pos == 22 and upstream.ref == "AGCCG" and upstream.alt[0] == "."), error_msg
+        self.assertTrue( upstream.pos == 22 and upstream.ref == "AGCCG" and upstream.alt[0] == "." )
         # Test deletion on start
         deletion = VCFRecord( "artificial_1", 1, None, ref[0], ["."], 230 )
         upstream = deletion.getMostUpstream( ref )
-        assert (upstream.pos == 1 and upstream.ref == "N" and upstream.alt[0] == "."), error_msg
+        self.assertTrue( upstream.pos == 1 and upstream.ref == "N" and upstream.alt[0] == "." )
         # Test deletion on end
         deletion = VCFRecord( "artificial_1", len(ref) - 1, None, ref[-2:], [ref[-2]], 230 )
         upstream = deletion.getMostUpstream( ref )
-        assert (upstream.pos == 33 and upstream.ref == "T" and upstream.alt[0] == "."), error_msg
+        self.assertTrue( upstream.pos == 33 and upstream.ref == "T" and upstream.alt[0] == "." )
         # Test fix insertion
         insertion = VCFRecord( "artificial_1", 4, None, "N", ["NGGTT"], 230 )
         upstream = insertion.getMostUpstream( ref )
-        assert (upstream.pos == 5 and upstream.ref == "." and upstream.alt[0] == "GGTT"), error_msg
+        self.assertTrue( upstream.pos == 5 and upstream.ref == "." and upstream.alt[0] == "GGTT" )
         # Test homopolymer insertion
         insertion = VCFRecord( "artificial_1", 18, None, "T", ["TT"], 230 )
         upstream = insertion.getMostUpstream( ref )
-        assert (upstream.pos == 17 and upstream.ref == "." and upstream.alt[0] == "T"), error_msg
+        self.assertTrue( upstream.pos == 17 and upstream.ref == "." and upstream.alt[0] == "T" )
         # Test complex insertion
         insertion = VCFRecord( "artificial_1", 25, None, "C", ["CGAgCC"], 230 )
         upstream = insertion.getMostUpstream( ref )
-        assert (upstream.pos == 22 and upstream.ref == "." and upstream.alt[0] == "AGCCG"), error_msg
+        self.assertTrue( upstream.pos == 22 and upstream.ref == "." and upstream.alt[0] == "AGCCG" )
         # Test insertion on start
         insertion = VCFRecord( "artificial_1", 1, None, ".", ["A"], 230 )
         upstream = insertion.getMostUpstream( ref )
-        assert (upstream.pos == 1 and upstream.ref == "." and upstream.alt[0] == "A"), error_msg
+        self.assertTrue( upstream.pos == 1 and upstream.ref == "." and upstream.alt[0] == "A" )
         # Test insertion on end
         insertion = VCFRecord( "artificial_1", len(ref), None, ref[-1], [ref[-1] + "G"], 230 )
         upstream = insertion.getMostUpstream( ref )
-        assert (upstream.pos == 34 and upstream.ref == "." and upstream.alt[0] == "G"), error_msg
+        self.assertTrue( upstream.pos == 34 and upstream.ref == "." and upstream.alt[0] == "G" )
 
-    def getMostDownstream(self):
+    def testGetMostDownstream(self):
         ref = "nnNNATGCCAaTgATGTTtTaAGCCGAGCCGAT" # length: 33
-        error_msg = "Error in method getMostDownstream() for class VCFRecord."
         # Test fix deletion
         deletion = VCFRecord( "artificial_1", 18, None, "TtTaAGC", ["T"], 230 )
         downstream = deletion.getMostDownstream( ref )
-        assert (downstream.pos == 19 and downstream.ref == "TTAAGC" and downstream.alt[0] == "."), error_msg
+        self.assertTrue( downstream.pos == 19 and downstream.ref == "TTAAGC" and downstream.alt[0] == "." )
         # Test homopolymer deletion
         deletion = VCFRecord( "artificial_1", 18, None, "TTT", ["T"], 230 )
         downstream = deletion.getMostDownstream( ref )
-        assert (downstream.pos == 19 and downstream.ref == "TT" and downstream.alt[0] == "."), error_msg
+        self.assertTrue( downstream.pos == 19 and downstream.ref == "TT" and downstream.alt[0] == "." )
         # Test complex deletion
         deletion = VCFRecord( "artificial_1", 25, None, "CGAgCC", ["C"], 230 )
         downstream = deletion.getMostDownstream( ref )
-        assert (downstream.pos == 28 and downstream.ref == "GCCGA" and downstream.alt[0] == "."), error_msg
+        self.assertTrue( downstream.pos == 28 and downstream.ref == "GCCGA" and downstream.alt[0] == "." )
         # Test deletion on start
         deletion = VCFRecord( "artificial_1", 1, None, ref[0], ["."], 230 )
         downstream = deletion.getMostDownstream( ref )
-        assert (downstream.pos == 4 and downstream.ref == "N" and downstream.alt[0] == "."), error_msg
+        self.assertTrue( downstream.pos == 4 and downstream.ref == "N" and downstream.alt[0] == "." )
         # Test deletion on end
         deletion = VCFRecord( "artificial_1", len(ref) - 1, None, ref[-2:], [ref[-2]], 230 )
         downstream = deletion.getMostDownstream( ref )
-        assert (downstream.pos == 33 and downstream.ref == "T" and downstream.alt[0] == "."), error_msg
+        self.assertTrue( downstream.pos == 33 and downstream.ref == "T" and downstream.alt[0] == "." )
         # Test fix insertion
         insertion = VCFRecord( "artificial_1", 4, None, "N", ["NGGTT"], 230 )
         downstream = insertion.getMostDownstream( ref )
-        assert (downstream.pos == 5 and downstream.ref == "." and downstream.alt[0] == "GGTT"), error_msg
+        self.assertTrue( downstream.pos == 5 and downstream.ref == "." and downstream.alt[0] == "GGTT" )
         # Test homopolymer insertion
         insertion = VCFRecord( "artificial_1", 18, None, "T", ["TT"], 230 )
         downstream = insertion.getMostDownstream( ref )
-        assert (downstream.pos == 21 and downstream.ref == "." and downstream.alt[0] == "T"), error_msg
+        self.assertTrue( downstream.pos == 21 and downstream.ref == "." and downstream.alt[0] == "T" )
         # Test complex insertion
         insertion = VCFRecord( "artificial_1", 25, None, "C", ["CGAgCC"], 230 )
         downstream = insertion.getMostDownstream( ref )
-        assert (downstream.pos == 33 and downstream.ref == "." and downstream.alt[0] == "GCCGA"), error_msg
+        self.assertTrue( downstream.pos == 33 and downstream.ref == "." and downstream.alt[0] == "GCCGA" )
         # Test insertion on start
         insertion = VCFRecord( "artificial_1", 1, None, ".", ["A"], 230 )
         downstream = insertion.getMostDownstream( ref )
-        assert (downstream.pos == 1 and downstream.ref == "." and downstream.alt[0] == "A"), error_msg
+        self.assertTrue( downstream.pos == 1 and downstream.ref == "." and downstream.alt[0] == "A" )
         # Test insertion on end
         insertion = VCFRecord( "artificial_1", len(ref), None, ref[-1], [ref[-1] + "G"], 230 )
         downstream = insertion.getMostDownstream( ref )
-        assert (downstream.pos == 34 and downstream.ref == "." and downstream.alt[0] == "G"), error_msg
-
-    def processTest(self):
-        self.getMostUpstream()
-        self.getMostDownstream()
+        self.assertTrue( downstream.pos == 34 and downstream.ref == "." and downstream.alt[0] == "G" )
 
 
 ########################################################################
@@ -135,5 +175,4 @@ class TestVCFRecord:
 #
 ########################################################################
 if __name__ == "__main__":
-    vcf_record = TestVCFRecord()
-    vcf_record.processTest()
+    unittest.main()
