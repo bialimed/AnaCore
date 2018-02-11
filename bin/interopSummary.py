0fa02411e888ab98ecf8998ef0c3715b5fc8e637
fescudie <escudie.frederic@wanadoo.fr>
2018-02-11 14:27:16 +0100

    Fix critical bug in errors rate retrieval in bin/interopSummary.py.

--- bin/interopSummary.py
+++ bin/interopSummary.py
@@ -19,7 +19,7 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2017 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -33,7 +33,7 @@ import argparse
 # FUNCTIONS
 #
 ########################################################################
-def getDistributionDictForCycles( val_by_cycle, percentile_step=25, precision=4 ):
+def getDistributionDictForCycles(val_by_cycle, percentile_step=25, precision=4):
     """
     @summary: Returns the distribution of values by cycle (min, max and percentiles).
     @param val_by_cycle: [dict] The values by cycle.
@@ -49,7 +49,7 @@ def getDistributionDictForCycles( val_by_cycle, percentile_step=25, precision=4 
         distrib['{:02}'.format(curr_percentile) + "_percentile"] = [round(numpy.percentile(val_by_cycle[cycle], curr_percentile), precision) for cycle in sorted(val_by_cycle)]
     return distrib
 
-def getDistributionDict( values, percentile_step=25, precision=4 ):
+def getDistributionDict(values, percentile_step=25, precision=4):
     """
     @summary: Returns the distribution of values (min, max and percentiles).
     @param values: [list] The values.
@@ -62,10 +62,10 @@ def getDistributionDict( values, percentile_step=25, precision=4 ):
         "max": round(max(values), precision)
     }
     for curr_percentile in range(percentile_step, 100, percentile_step):
-        distrib['{:02}'.format(curr_percentile) + "_percentile"] = round( numpy.percentile(values, curr_percentile), precision )
+        distrib['{:02}'.format(curr_percentile) + "_percentile"] = round(numpy.percentile(values, curr_percentile), precision)
     return distrib
 
-def getrunMetrics( interop_path ):
+def getrunMetrics(interop_path):
     """
     @summary: Returns summary of run metrics from InterOp file.
     @param interop_path: [str] "The interop file to process (format: TXT). This file comes from the usage of dumptext on Illumina's interOp folder (cf. package on http://illumina.github.io/interop/index.html).
@@ -90,32 +90,32 @@ def getrunMetrics( interop_path ):
                 is_error_section = False
                 if line.strip() == "# Tile,1":
                     is_tiles_section = True
-                    trash = FH.readline() # Column Count: 10
-                    tiles_header = [col.strip() for col in FH.readline().split(",")] # Lane,Tile,Read,ClusterCount,ClusterCountPF,Density,DensityPF,Aligned,Prephasing,Phasing
+                    trash = FH.readline()  # Column Count: 10
+                    tiles_header = [col.strip() for col in FH.readline().split(",")]  # Lane,Tile,Read,ClusterCount,ClusterCountPF,Density,DensityPF,Aligned,Prephasing,Phasing
                 elif line.strip() == "# Error,1":
                     is_error_section = True
-                    trash = FH.readline() # Column Count: 4
-                    tiles_header = [col.strip() for col in FH.readline().split(",")] # Lane,Tile,Cycle,ErrorRate
+                    trash = FH.readline()  # Column Count: 4
+                    tiles_header = [col.strip() for col in FH.readline().split(",")]  # Lane,Tile,Cycle,ErrorRate
             elif is_tiles_section:
-                tile_metrics = {tiles_header[col_idx]:col.strip() for col_idx, col in enumerate(line.split(","))}
-                tiles["density"].append( float(tile_metrics["Density"]) )
-                tiles["density_PF"].append( float(tile_metrics["DensityPF"]) )
-                tiles["cluster_count"].append( int(float(tile_metrics["ClusterCount"])) )
-                tiles["cluster_count_PF"].append( int(float(tile_metrics["ClusterCountPF"])) )
+                tile_metrics = {tiles_header[col_idx]: col.strip() for col_idx, col in enumerate(line.split(","))}
+                tiles["density"].append(float(tile_metrics["Density"]))
+                tiles["density_PF"].append(float(tile_metrics["DensityPF"]))
+                tiles["cluster_count"].append(int(float(tile_metrics["ClusterCount"])))
+                tiles["cluster_count_PF"].append(int(float(tile_metrics["ClusterCountPF"])))
             elif is_error_section:
-                tile_metrics = {tiles_header[col_idx]:col.strip() for col_idx, col in enumerate(line.split(","))}
-                cycle = tile_metrics["Cycle"]
+                tile_metrics = {tiles_header[col_idx]: col.strip() for col_idx, col in enumerate(line.split(","))}
+                cycle = int(tile_metrics["Cycle"])
                 if cycle not in tiles["error_rate"]:
                     tiles["error_rate"][cycle] = list()
-                tiles["error_rate"][cycle].append( float(tile_metrics["ErrorRate"]) )
+                tiles["error_rate"][cycle].append(float(tile_metrics["ErrorRate"]))
 
     # Process summary
     tiles_summary = {
-        "density": getDistributionDict( tiles["density"], 5, 0 ),
-        "density_PF": getDistributionDict( tiles["density_PF"], 5, 0 ),
-        "cluster_count": getDistributionDict( tiles["cluster_count"], 5, 0 ),
-        "cluster_count_PF": getDistributionDict( tiles["cluster_count_PF"], 5, 0 ),
-        "error_rate": getDistributionDictForCycles( tiles["error_rate"], 5, 4 )
+        "density": getDistributionDict(tiles["density"], 5, 0),
+        "density_PF": getDistributionDict(tiles["density_PF"], 5, 0),
+        "cluster_count": getDistributionDict(tiles["cluster_count"], 5, 0),
+        "cluster_count_PF": getDistributionDict(tiles["cluster_count_PF"], 5, 0),
+        "error_rate": getDistributionDictForCycles(tiles["error_rate"], 5, 4)
     }
 
     return tiles_summary
@@ -128,15 +128,17 @@ def getrunMetrics( interop_path ):
 ########################################################################
 if __name__ == "__main__":
     # Manage parameters
-    parser = argparse.ArgumentParser( description="Summarizes an Illumina's run metrics from an InterOp file." )
-    parser.add_argument( '-v', '--version', action='version', version=__version__ )
-    group_input = parser.add_argument_group( 'Inputs' ) # Inputs
-    group_input.add_argument( '-i', '--interop-file', required=True, help="The interop file to process (format: TXT). This file comes from the usage of dumptext on Illumina's interOp folder (cf. package on http://illumina.github.io/interop/index.html)." )
-    group_output = parser.add_argument_group( 'Outputs' ) # Outputs
-    group_output.add_argument( '-o', '--output-file', default="interOp_summary.json", help='Path to the output file (format: JSON). [Default: %(default)s]')
+    parser = argparse.ArgumentParser(description="Summarizes an Illumina's run metrics from an InterOp file.")
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+    group_input = parser.add_argument_group('Inputs')  # Inputs
+    group_input.add_argument('-i', '--interop-file', required=True, help="The interop file to process (format: TXT). This file comes from the usage of dumptext on Illumina's interOp folder (cf. package on http://illumina.github.io/interop/index.html).")
+    group_output = parser.add_argument_group('Outputs')  # Outputs
+    group_output.add_argument('-o', '--output-file', default="interOp_summary.json", help='Path to the output file (format: JSON). [Default: %(default)s]')
     args = parser.parse_args()
 
     # Process
-    run_metrics = getrunMetrics( args.interop_file )
+    run_metrics = getrunMetrics(args.interop_file)
     with open(args.output_file, "w") as FH_out:
-        FH_out.write( json.dumps(run_metrics, default=lambda o: o.__dict__, sort_keys=True) )
+        FH_out.write(
+            json.dumps(run_metrics, default=lambda o: o.__dict__, sort_keys=True)
+        )
