a3cf26d08cf2ea32bad0bcac286c9ed898b361cc
fescudie <escudie.frederic@wanadoo.fr>
2018-03-11 17:11:58 +0100

    Fix bug when CSQ is empty or missing for a record in lib/VEPvcf.py.

--- lib/VEPvcf.py
+++ lib/VEPvcf.py
@@ -18,7 +18,7 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2017 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -33,50 +33,57 @@ class VEPVCFIO(VCFIO):
         self.CSQ_titles = list()
         super().__init__(*args, **kwargs)
 
-    def _parseHeader( self ):
+    def _parseHeader(self):
         super()._parseHeader()
         if "CSQ" in self.info:
             # Get CSQ fields
-            match = re.search( "Format: ([^ ]+)", self.info["CSQ"]["description"] )
+            match = re.search("Format: ([^ ]+)", self.info["CSQ"]["description"])
             if match is None:
                 raise Exception("The CSQ description cannot be parsed in file " + self.filepath)
             self.CSQ_titles = match.group(1).split("|")
 
     def _parseLine(self):
         record = super()._parseLine()
-        self._parseCSQ( record )
+        self._parseCSQ(record)
         return record
 
     def _parseCSQ(self, record):
-        for annot_idx, annot in enumerate(record.info["CSQ"]):
-            annot_val = dict()
-            for field_idx, field_value in enumerate(annot.split("|")):
-                csq_key = self.CSQ_titles[field_idx]
-                if field_value.strip() != "":
-                    annot_val[csq_key] = field_value.strip()
-                else:
-                    annot_val[csq_key] = None
-            record.info["CSQ"][annot_idx] = annot_val
+        if "CSQ" in self.info:
+            if "CSQ" not in record.info or record.info["CSQ"] is None:  # CSQ is empty
+                record.info["CSQ"] = list()
+            else:  # The variant has consequence(s)
+                for annot_idx, annot in enumerate(record.info["CSQ"]):
+                    annot_val = dict()
+                    for field_idx, field_value in enumerate(annot.split("|")):
+                        csq_key = self.CSQ_titles[field_idx]
+                        if field_value.strip() != "":
+                            annot_val[csq_key] = field_value.strip()
+                        else:
+                            annot_val[csq_key] = None
+                    record.info["CSQ"][annot_idx] = annot_val
 
-    def copyHeader( self, model ):
+    def copyHeader(self, model):
         """
         @summary: Copy header fields from the specified VCF.
         @param model: [VEPVCFIO] The VCF source.
         """
-        super().copyHeader( model )
+        super().copyHeader(model)
         self.CSQ_titles = model.CSQ_titles
 
     def recToVCFLine(self, record):
-        std_record = deepcopy( record )
+        std_record = deepcopy(record)
         if "CSQ" in record.info:
-            csq_fields = list()
-            for annot in record.info["CSQ"]:
-                annot_fields = list()
-                for csq_key in self.CSQ_titles:
-                    if annot[csq_key] is None:
-                        annot_fields.append("")
-                    else:
-                        annot_fields.append( str(annot[csq_key]) )
-                csq_fields.append( "|".join(annot_fields) )
-            std_record.info["CSQ"] = csq_fields
+            if record.info["CSQ"] is None or len(record.info["CSQ"]) == 0:  # CSQ is empty
+                std_record.info.pop('CSQ', None)
+            else:  # The variant has consequence(s)
+                csq_fields = list()
+                for annot in record.info["CSQ"]:
+                    annot_fields = list()
+                    for csq_key in self.CSQ_titles:
+                        if annot[csq_key] is None:
+                            annot_fields.append("")
+                        else:
+                            annot_fields.append(str(annot[csq_key]))
+                    csq_fields.append("|".join(annot_fields))
+                std_record.info["CSQ"] = csq_fields
         return super().recToVCFLine(std_record)
