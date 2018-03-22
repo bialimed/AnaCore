51479e0bfadbfa1e51099effd46036685cb8d487
fescudie <escudie.frederic@wanadoo.fr>
2018-03-22 15:27:53 +0100

    Add setup.py for lib package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='anacore',
+    version='2.0',
+    description='Libraries to read/write/process standard files in NGS.',
+    long_description='Anapath Core is package containing libraries to read/write/process standard files in NGS. It has been developped for Anatomo-Cytopathologie department of IUCT Oncopole.',
+    author='Frederic Escudie',
+    author_email='escudie.frederic@iuct-oncopole.fr',
+    packages=["anacore"]
+)
