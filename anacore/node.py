1f480d9f846c3532e979be5e6e5530655c8954ef
fescudie <escudie.frederic@wanadoo.fr>
2018-07-27 10:42:37 +0200

    Change author email address.

--- anacore/node.py
+++ anacore/node.py
@@ -19,7 +19,7 @@ __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2015 INRA'
 __license__ = 'GNU General Public License'
 __version__ = '1.0.3'
-__email__ = 'frederic.escudie@iuct-oncopole.fr'
+__email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
 import json
--- anacore/region.py
+++ anacore/region.py
@@ -19,7 +19,7 @@ __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2017 IUCT-O'
 __license__ = 'GNU General Public License'
 __version__ = '1.2.0'
-__email__ = 'frederic.escudie@iuct-oncopole.fr'
+__email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
 
