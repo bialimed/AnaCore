fb35e1218b44a8ca10cafcca3ce7c4223e496325
fescudie <escudie.frederic@wanadoo.fr>
2018-12-20 11:22:50 +0100

    Add BEDIO.getMaxNbCol in lib/anacore/bed.py.

--- lib/anacore/bed.py
+++ lib/anacore/bed.py
@@ -18,7 +18,7 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2017 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -27,8 +27,38 @@ from anacore.abstractFile import AbstractFile
 
 
 class BEDRecord(Region):
+    """Class to manage a BED record."""
+
     def __init__(self, chrom=None, start=None, end=None, name=None, score=None, strand=None, thickStart=None, thickEnd=None, itemRgb=None, blockCount=None, blockSizes=None, blockStarts=None):
         """
+        Build and return an instance of BEDRecord.
+
+        :param chrom: The name of the chromosome on which the annotation has been defined.
+        :type chrom: str
+        :param start: The start position on the reference. This position is 1-based and ascending (start <= end).
+        :type start: int
+        :param end: The end position on the reference. This position is 1-based and ascending (start <= end). [Default: start]
+        :type end: int
+        :param name: The name of the annotation.
+        :type name: str
+        :param score: A score between 0 and 1000
+        :type score: int
+        :param strand: The strand of the annotation ("+" or "-").
+        :type strand: str
+        :param thickStart: The starting position at which the feature is drawn thickly (for example, the start codon in gene displays). This position is 1-based and ascending (start <= end).
+        :type thickStart: int
+        :param thickEnd: The ending position at which the feature is drawn thickly (for example the stop codon in gene displays). This position is 1-based and ascending (start <= end).
+        :type thickEnd: int
+        :param itemRgb: An RGB value of the form R,G,B (e.g. 255,0,0).
+        :type itemRgb: list
+        :param blockCount: The number of blocks (exons) in the BED line.
+        :type blockCount: int
+        :param blockSizes: list of the block sizes
+        :type blockSizes: list
+        :param blockStarts: list of block starts. All of the blockStart positions should be calculated relative to chromStart.
+        :type blockStarts: list
+        :return: The new instance.
+        :rtype: BEDRecord
         """
         Region.__init__(self, start, end, strand, chrom, name)
         self.score = score
@@ -53,16 +83,34 @@ class BEDRecord(Region):
         else:
             object.__setattr__(self, attr_name, attr_value)
 
+
 class BEDIO(AbstractFile):
+    """Class to read and write in BED file (https://genome.ucsc.edu/FAQ/FAQformat.html#format1)."""
+
     def __init__(self, filepath, mode="r", write_nb_col=3):
+        """
+        Build and return an instance of BEDIO.
+
+        :param filepath: The filepath.
+        :type filepath: str.
+        :param mode: Mode to open the file ('r', 'w', 'a').
+        :type mode: str.
+        :param write_nb_col: The number of column in BED file when the mode is in ["a", "w"].
+        :type write_nb_col: int.
+        :return: The new instance.
+        :rtype: BEDIO
+        """
         AbstractFile.__init__(self, filepath, mode)
         self._write_nb_col = write_nb_col
 
     def BEDRecordToBEDLine(self, record):
         """
-        @summary : Returns the record in BED format.
-        @param record : [BEDRecord] The sequence to process.
-        @return : [str] The BED line.
+        Return the record in BED format.
+
+        :param record: The record to process.
+        :type record: BEDRecord.
+        :return: The BED line corresponding to the record.
+        :rtype: str
         """
         str_fmt = "\t".join(["{}" for idx in range(self._write_nb_col)])
         line = str_fmt.format(
@@ -83,9 +131,12 @@ class BEDIO(AbstractFile):
 
     def isRecordLine(self, line):
         """
-        @summary: Returns True if the line corresponds to a record (it is not a comment or an header line).
-        @param line: [str] The evaluated line.
-        @return: [bool] True if the line corresponds to a record.
+        Return True if the line corresponds to a record (it is not a comment or an header line).
+
+        :param line: The evaluated line.
+        :type line: str.
+        :return: True if the line corresponds to a record.
+        :rtype: bool
         """
         is_record = True
         if line.startswith("browser ") or line.startswith("track ") or line.startswith("#"):
@@ -95,8 +146,10 @@ class BEDIO(AbstractFile):
     @staticmethod
     def isValid(filepath):
         """
-        @summary: Returns True is the file can be a BED.
-        @return: [bool] True if the file can be a BED.
+        Return True if the file can be a BED file.
+
+        :return: True if the file can be a BED file.
+        :rtype: bool
         """
         is_valid = False
         try:
@@ -105,57 +158,84 @@ class BEDIO(AbstractFile):
                 for record in FH_in:
                     if record_idx >= 10:
                         break
-                    if record.strand is not None and record.strand not in ["+", "-", "."]:
+                    if record.strand is not None and record.strand not in ["+", "-", "."]:  # Check strand
                         raise IOError(
                             "The line {} in \"{}\" cannot be parsed by {}.\nLine content: {}".format(
-                                self.current_line_nb, self.filepath, self.__class__.__name__, self.current_line
+                                FH_in.current_line_nb, FH_in.filepath, FH_in.__class__.__name__, FH_in.current_line
                             )
                         )
-                    #~ if record.itemRgb is not None and len(record.itemRgb.split(",")) != 3:
-                        #~ raise IOError( "The line " + str(self.current_line_nb) + " in '" + self.filepath + "' cannot be parsed by " + self.__class__.__name__ + ".\n" +
-                               #~ "Line content : " + self.current_line )
+                    # if record.itemRgb is not None and len(record.itemRgb.split(",")) != 3:
+                    #     raise IOError(
+                    #         "The line {} in \"{}\" cannot be parsed by {}.\nLine content: {}".format(
+                    #             FH_in.current_line_nb, FH_in.filepath, FH_in.__class__.__name__, FH_in.current_line
+                    #         )
+                    #     )
                     record_idx += 1
                 is_valid = True
-        except:
+        except Exception:
             pass
         return is_valid
 
     def _parseLine(self):
         """
-        @summary: Returns a structured record from the BED current line.
-        @return: [BEDrecord] The record.
+        Return a structured record from the current line.
+
+        :return: The record defined by the current line.
+        :rtype: BEDrecord
         """
         fields = [elt.strip() for elt in self.current_line.split('\t')]
         fields[1] = int(fields[1]) + 1  # Start in BED is 0-based
         fields[2] = int(fields[2])
-        if len(fields) >= 5:
+        nb_col = len(fields)
+        if nb_col >= 5:
             fields[4] = None if fields[4] == "." else int(fields[4])  # A score between 0 and 1000. If the track line useScore attribute is set to 1 for this annotation data set, the score value will determine the level of gray in which this feature is displayed (higher numbers = darker gray).
-            if len(fields) >= 7:
+            if nb_col >= 7:
                 fields[6] = int(fields[6]) + 1  # The starting position at which the feature is drawn thickly (for example, the start codon in gene displays). When there is no thick part, thickStart and thickEnd are usually set to the chromStart position.
-                if len(fields) >= 8:
+                if nb_col >= 8:
                     fields[7] = int(fields[7])  # The ending position at which the feature is drawn thickly (for example the stop codon in gene displays).
-                    if len(fields) >= 10:
+                    if nb_col >= 10:
                         fields[9] = int(fields[9])  # The number of blocks (exons) in the BED line.
-                        if len(fields) >= 11:
+                        if nb_col >= 11:
                             fields[10] = [int(block) for block in fields[10].split(",")]  # A comma-separated list of the block sizes. The number of items in this list should correspond to blockCount.
-                            if len(fields) >= 12:
+                            if nb_col >= 12:
                                 fields[11] = [int(block) for block in fields[11].split(",")]  # A comma-separated list of block starts. All of the blockStart positions should be calculated relative to chromStart. The number of items in this list should correspond to blockCount.
         return BEDRecord(*fields)
 
     def write(self, bed_record):
         """
-        @summary: Writes record line in file.
-        @param bed_record: [BEDRecord] The record.
+        Write record line in file.
+
+        :param bed_record: The record to write.
+        :type bed_record: BEDRecord
         """
         self.file_handle.write(self.BEDRecordToBEDLine(bed_record) + "\n")
         self.current_line_nb += 1
 
+    @staticmethod
+    def getMaxNbCol(in_bed):
+        """
+        Return the maximum number of fields in BED file. The number of fields per line must be consistent throughout any single set of data in an annotation track.
+
+        :param in_bed: The path to the file (format: BED).
+        :type in_bed: str
+        :return: The maximum number of fields from the first line of the BED file.
+        :rtype: int
+        """
+        nb_col = -1
+        with BEDIO(in_bed) as FH_in:
+            for record in FH_in:
+                nb_col = max(nb_col, FH_in.current_line.count("\t") + 1)
+        return None if nb_col == -1 else nb_col
+
 
 def getAreas(in_bed):
     """
-    @summary: Returns the list of areas from a BED file.
-    @param input_areas: [str] The path to the areas description (format: BED).
-    @returns: [RegionList] The list of areas.
+    Return the list of areas from a BED file.
+
+    :param in_bed: The path to the areas description (format: BED).
+    :type in_bed: str
+    :return: The list of areas.
+    :rtype: region.RegionList
     """
     areas = RegionList()
     with BEDIO(in_bed) as FH_panel:
@@ -165,9 +245,12 @@ def getAreas(in_bed):
 
 def getAreasByChr(in_bed):
     """
-    @summary: Returns from a BED file the list of areas by chromosome.
-    @param input_areas: [str] The path to the areas description (format: BED).
-    @returns: [dict] The list of areas by chromosome (each list is an instance of Regionlist).
+    Return by chromosome the list of areas from a BED file.
+
+    :param in_bed: The path to the areas description (format: BED).
+    :type in_bed: str
+    :return: The list of areas by chromosome (each list is an instance of region.Regionlist).
+    :rtype: dict
     """
     areas_by_chr = dict()
     for curr_area in getAreas(in_bed):
