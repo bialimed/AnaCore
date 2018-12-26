bialimed/AnaCore

commit 1331cb251fcd5bacd0a5c010ba2994cbdb6292bb

Replace lib/anacore/VEPvcf.py by lib/anacore/annotVcf.py.

bin/VCFToSV.py
@@ -19,7 +19,7 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2017 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.4.0'
+__version__ = '1.5.0'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -31,7 +31,7 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 LIB_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "lib"))
 sys.path.append(LIB_DIR)
 
-from anacore.VEPvcf import VEPVCFIO, getAlleleRecord
+from anacore.annotVcf import VEPVCFIO, getAlleleRecord
 
 
 ########################################################################

bin/VCFToJSON.py
@@ -19,7 +19,7 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2017 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -33,7 +33,7 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 LIB_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "lib"))
 sys.path.append(LIB_DIR)
 
-from anacore.VEPvcf import VEPVCFIO, getAlleleRecord
+from anacore.annotVcf import VEPVCFIO, getAlleleRecord
 
 
 ########################################################################

bin/filterVCFAnnotOnRNA.py
@@ -19,7 +19,7 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2017 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.1.1'
+__version__ = '1.2.0'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -31,7 +31,7 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 LIB_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "lib"))
 sys.path.append(LIB_DIR)
 
-from anacore.VEPvcf import VEPVCFIO
+from anacore.annotVcf import VEPVCFIO
 
 
 ########################################################################

bin/filterVCFOnAnnot.py
@@ -19,7 +19,7 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2017 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.5.1'
+__version__ = '1.6.0'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -31,7 +31,7 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 LIB_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "lib"))
 sys.path.append(LIB_DIR)
 
-from anacore.VEPvcf import VEPVCFIO, getAlleleRecord
+from anacore.annotVcf import VEPVCFIO, getAlleleRecord
 
 
 ########################################################################

bin/prototype/Acinome/geneMutated.py
@@ -8,7 +8,7 @@ if os.getenv('PYTHONPATH') is None: os.environ['PYTHONPATH'] = LIB_DIR
 else: os.environ['PYTHONPATH'] = os.environ['PYTHONPATH'] + os.pathsep + LIB_DIR
 
 from GTFI import *
-from VEPvcf import VEPVCFIO, getAlleleRecord
+from annotVcf import VEPVCFIO, getAlleleRecord
 
 def getGenesByChrom( gtf_file ):
     genes_by_chr = dict()
@@ -59,7 +59,7 @@ for idx_vcf, current_vcf in enumerate(sys.argv[2:]):
                     if "AF" in allele_record.samples[spl_name]:
                         frequencies = allele_record.samples[spl_name]["AF"]
                         print(frequencies)
-                        if FH_vcf.info["AF"]["number_tag"] == "R": 
+                        if FH_vcf.info["AF"]["number_tag"] == "R":
                             allele_freq = frequencies[1]
                         else:
                             allele_freq = frequencies[0]

bin/prototype/Acinome/idsToCoverage.py
@@ -10,7 +10,7 @@ sys.path.append(LIB_DIR)
 if os.getenv('PYTHONPATH') is None: os.environ['PYTHONPATH'] = LIB_DIR
 else: os.environ['PYTHONPATH'] = os.environ['PYTHONPATH'] + os.pathsep + LIB_DIR
 
-from VEPvcf import VEPVCFIO, getAlleleRecord
+from annotVcf import VEPVCFIO, getAlleleRecord
 
 
 if __name__ == "__main__":
@@ -35,7 +35,7 @@ if __name__ == "__main__":
                         "chrom": chrom,
                         "pos": pos,
                         "alleles": [],
-                        "coverage": dict()                   
+                        "coverage": dict()
                     }
                 variants[pos_id]["alleles"].append( allele_id )
 

lib/anacore/VEPvcf.py
@@ -1,89 +1,0 @@
-#
-# Copyright (C) 2017 IUCT-O
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-
-__author__ = 'Frederic Escudie'
-__copyright__ = 'Copyright (C) 2017 IUCT-O'
-__license__ = 'GNU General Public License'
-__version__ = '1.2.1'
-__email__ = 'escudie.frederic@iuct-oncopole.fr'
-__status__ = 'prod'
-
-import re
-from copy import deepcopy
-from anacore.vcf import *
-
-
-
-class VEPVCFIO(VCFIO):
-    def __init__(self, *args, **kwargs):
-        self.CSQ_titles = list()
-        super().__init__(*args, **kwargs)
-
-    def _parseHeader(self):
-        super()._parseHeader()
-        if "CSQ" in self.info:
-            # Get CSQ fields
-            match = re.search("Format: ([^ ]+)", self.info["CSQ"]["description"])
-            if match is None:
-                raise Exception("The CSQ description cannot be parsed in file " + self.filepath)
-            self.CSQ_titles = match.group(1).split("|")
-
-    def _parseLine(self):
-        record = super()._parseLine()
-        self._parseCSQ(record)
-        return record
-
-    def _parseCSQ(self, record):
-        if "CSQ" in self.info:
-            if "CSQ" not in record.info or record.info["CSQ"] is None:  # CSQ is empty
-                record.info["CSQ"] = list()
-            else:  # The variant has consequence(s)
-                for annot_idx, annot in enumerate(record.info["CSQ"]):
-                    annot_val = dict()
-                    for field_idx, field_value in enumerate(annot.split("|")):
-                        csq_key = self.CSQ_titles[field_idx]
-                        if field_value.strip() != "":
-                            annot_val[csq_key] = field_value.strip()
-                        else:
-                            annot_val[csq_key] = None
-                    record.info["CSQ"][annot_idx] = annot_val
-
-    def copyHeader(self, model):
-        """
-        @summary: Copy header fields from the specified VCF.
-        @param model: [VEPVCFIO] The VCF source.
-        """
-        super().copyHeader(model)
-        self.CSQ_titles = model.CSQ_titles
-
-    def recToVCFLine(self, record):
-        std_record = deepcopy(record)
-        if "CSQ" in record.info:
-            if record.info["CSQ"] is None or len(record.info["CSQ"]) == 0:  # CSQ is empty
-                std_record.info.pop('CSQ', None)
-            else:  # The variant has consequence(s)
-                csq_fields = list()
-                for annot in record.info["CSQ"]:
-                    annot_fields = list()
-                    for csq_key in self.CSQ_titles:
-                        if annot[csq_key] is None:
-                            annot_fields.append("")
-                        else:
-                            annot_fields.append(str(annot[csq_key]))
-                    csq_fields.append("|".join(annot_fields))
-                std_record.info["CSQ"] = csq_fields
-        return super().recToVCFLine(std_record)

lib/anacore/annotVcf.py
@@ -1,0 +1,144 @@
+#
+# Copyright (C) 2018 IUCT-O
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = 'Frederic Escudie'
+__copyright__ = 'Copyright (C) 2018 IUCT-O'
+__license__ = 'GNU General Public License'
+__version__ = '1.0.0'
+__email__ = 'escudie.frederic@iuct-oncopole.fr'
+__status__ = 'prod'
+
+import re
+from copy import deepcopy
+from anacore.vcf import *
+
+
+class AnnotVCFIO(VCFIO):
+    """Manage VCF file containing variants annotations."""
+
+    def __init__(self, filepath, mode="r", annot_field="ANN"):
+        """
+        Return instance of AnnotVCFIO.
+
+        :param filepath: The filepath.
+        :type filepath: str
+        :param mode: Mode to open the file ('r', 'w', 'a').
+        :type mode: str
+        :param annot_field: The tag for the field used to store annotations. [Default: ANN]
+        :type annot_field: str
+        :return: The new instance.
+        :rtype: AnnotVCFIO
+        """
+        self.annot_field = annot_field
+        self.ANN_titles = list()
+        super().__init__(filepath, mode)
+
+    def copyHeader(self, model):
+        """
+        Copy header fields from the specified VCF.
+
+        :param model: The source.
+        :type model: vcf.VCFIO
+        """
+        super().copyHeader(model)
+        if hasattr(model, 'ANN_titles'):
+            self.ANN_titles = model.ANN_titles
+
+    def _parseAnnot(self, record):
+        """
+        Transform annotations stored as string in record to a structured annotations (list of dict).
+
+        :param record: The record containing the annotations.
+        :type record: vcf.VCFRecord
+        """
+        if self.annot_field in self.info:
+            if self.annot_field not in record.info or record.info[self.annot_field] is None:  # ANN is empty
+                record.info[self.annot_field] = list()
+            else:  # The variant has consequence(s)
+                for annot_idx, annot in enumerate(record.info[self.annot_field]):
+                    annot_val = dict()
+                    for field_idx, field_value in enumerate(annot.split("|")):
+                        csq_key = self.ANN_titles[field_idx]
+                        if field_value.strip() != "":
+                            annot_val[csq_key] = field_value.strip()
+                        else:
+                            annot_val[csq_key] = None
+                    record.info[self.annot_field][annot_idx] = annot_val
+
+    def _parseHeader(self):
+        """Retrieve and store annotations subfields titles and order from the annotation description in INFO header."""
+        super()._parseHeader()
+        if self.annot_field in self.info:
+            # Get ANN fields
+            match = re.search("Format: ([^ ]+)", self.info[self.annot_field]["description"])
+            if match is None:
+                raise Exception("The {} description cannot be parsed in file {}.".format(self.annot_field, self.filepath))
+            self.ANN_titles = match.group(1).split("|")
+
+    def _parseLine(self):
+        """
+        Return the record corresponding to the current line.
+
+        :return: The record corresponding to the current line.
+        :rtype: vcf.VCFRecord
+        """
+        record = super()._parseLine()
+        self._parseAnnot(record)
+        return record
+
+    def recToVCFLine(self, record):
+        """
+        Return the record in VCF format.
+
+        :param record: The record to process.
+        :type record: VCFRecord
+        :return: The VCF line.
+        :rtype: str
+        """
+        std_record = deepcopy(record)
+        if self.annot_field in record.info:
+            if record.info[self.annot_field] is None or len(record.info[self.annot_field]) == 0:  # ANN is empty
+                std_record.info.pop(self.annot_field, None)
+            else:  # The variant has consequence(s)
+                csq_fields = list()
+                for annot in record.info[self.annot_field]:
+                    annot_fields = list()
+                    for csq_key in self.ANN_titles:
+                        if annot[csq_key] is None:
+                            annot_fields.append("")
+                        else:
+                            annot_fields.append(str(annot[csq_key]))
+                    csq_fields.append("|".join(annot_fields))
+                std_record.info[self.annot_field] = csq_fields
+        return super().recToVCFLine(std_record)
+
+
+class VEPVCFIO(AnnotVCFIO):
+    """Manage VCF file containing variants annotations produced by VEP and stored in CSQ field."""
+
+    def __init__(self, filepath, mode="r"):
+        """
+        Return instance of VEPVCFIO.
+
+        :param filepath: The filepath.
+        :type filepath: str
+        :param mode: Mode to open the file ('r', 'w', 'a').
+        :type mode: str
+        :return: The new instance.
+        :rtype: VEPVCFIO
+        """
+        super().__init__(filepath, mode, "CSQ")

commit 4d6d927988dd6e32ee1b4177d2796c9c454d29d7

Fix bug introduced by commit 1331cb2.

bin/VCFToSV.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
             "Filters",
             "Alt_alleles_frequencies",
             args.separator.join(FH_vcf.samples),
-            args.separator.join(FH_vcf.CSQ_titles),
+            args.separator.join(FH_vcf.ANN_titles),
             sep=args.separator
         )
         for record in FH_vcf:
@@ -105,7 +105,7 @@ if __name__ == "__main__":
             if len(record.info["CSQ"]) == 0:  # Record without consequence
                 print(
                     args.separator.join(record_fields),
-                    args.separator.join(["" for col in FH_vcf.CSQ_titles]),
+                    args.separator.join(["" for col in FH_vcf.ANN_titles]),
                     sep=args.separator
                 )
             else:  # Record with at least one consequence
@@ -116,7 +116,7 @@ if __name__ == "__main__":
                         start_fields = record_fields
                     # Pre-process consequence display
                     csq_values = list()
-                    for title in FH_vcf.CSQ_titles:
+                    for title in FH_vcf.ANN_titles:
                         if title not in csq or csq[title] is None:
                             csq_values.append("")
                         else: