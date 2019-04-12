3eae5a44ead3c0a9452e77d148291e2083f298a2
fescudie <escudie.frederic@iuct-oncopole.fr>
2019-04-12 14:13:53 +0200

    Add management of string representing a number in web/components/thresholdAlert.js.

--- web/components/thresholdAlert.js
+++ web/components/thresholdAlert.js
@@ -1,5 +1,5 @@
 /*!
-  * thresholdAlert component v1.1.0
+  * thresholdAlert component v1.2.0
   * Copyright 2018 IUCT-O
   * Author Frederic Escudie
   * Licensed under GNU General Public License
@@ -9,44 +9,43 @@
 class ThresholdAlert {
     constructor(rules, text_pre, text_post="", value_suffix="") {
         this.text_pre = text_pre
-		this.rules = rules
+        this.rules = rules
         this.text_post = text_post
         this.value_suffix = value_suffix
-	}
+    }
 }
 
 
 Vue.component('threshold-alert', {
-	props: {
-		'threshold': {
+    props: {
+        'threshold': {
             required: true,
             type: Object
         },
         'value': {
-            required: true,
-            type: Number
+            required: true  // Must be a number or a string representing a number
         }
-	},
-	methods: {
-		getThresholdCategory: function (thresholds, eval_value) {
-			let value_category = null ;
-			const asc_thresholds = Object.keys(thresholds).sort(function (a, b) {  return parseFloat(a) - parseFloat(b)  })
-			asc_thresholds.forEach(function(curr_threshold){
-				if( parseFloat(curr_threshold) <= eval_value ){
-					value_category = thresholds[curr_threshold]
-				}
-			});
-			return( value_category )
-		}
-	},
-	computed: {
-		category_classes: function(){
-			const category_classes = "alert alert-" + this.getThresholdCategory( this.threshold.rules, this.value )
-			return( category_classes )
-		}
-	},
-	template:
-		`<div :class="category_classes" role="alert">
-			{{threshold.text_pre}} <span class="right-badge">{{value}}{{threshold.value_suffix}}</span> {{threshold.text_post}}
+    },
+    methods: {
+        getThresholdCategory: function (thresholds, eval_value) {
+            let value_category = null ;
+            const asc_thresholds = Object.keys(thresholds).sort(function (a, b) {  return parseFloat(a) - parseFloat(b)  })
+            asc_thresholds.forEach(function(curr_threshold){
+                if( parseFloat(curr_threshold) <= parseFloat(eval_value) ){
+                    value_category = thresholds[curr_threshold]
+                }
+            });
+            return( value_category )
+        }
+    },
+    computed: {
+        category_classes: function(){
+            const category_classes = "alert alert-" + this.getThresholdCategory( this.threshold.rules, this.value )
+            return( category_classes )
+        }
+    },
+    template:
+        `<div :class="category_classes" role="alert">
+            {{threshold.text_pre}} <span class="right-badge">{{value}}{{threshold.value_suffix}}</span> {{threshold.text_post}}
         </div>`
 })
