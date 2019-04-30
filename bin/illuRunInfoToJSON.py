2b2ba0fd7dcc9d94b013e97ca4af713dab962620
fescudie <escudie.frederic@wanadoo.fr>
2019-04-30 13:35:42 +0200

    Add getRunFolderInfo in lib/anacore/illumina.py and bin/illuRunInfoToJSON.py.

---
+++ bin/illuRunInfoToJSON.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2019 IUCT-O
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = 'Frederic Escudie'
+__copyright__ = 'Copyright (C) 2019 IUCT-O'
+__license__ = 'GNU General Public License'
+__version__ = '1.0.0'
+__email__ = 'escudie.frederic@iuct-oncopole.fr'
+__status__ = 'prod'
+
+import os
+import sys
+import json
+import logging
+import argparse
+from datetime import datetime
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+LIB_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "lib"))
+sys.path.append(LIB_DIR)
+
+from anacore.illumina import getRunFolderInfo
+
+
+########################################################################
+#
+# FUNCTIONS
+#
+########################################################################
+class DateTimeEncoder(json.JSONEncoder):
+    """JSON encoder for datetime"""
+    def default(self, obj):
+        encoded = None
+        if isinstance(obj, datetime):
+            encoded = obj.timestamp()
+        else:
+            encoded = json.JSONEncodzer.default(self, obj)
+        return encoded
+
+
+########################################################################
+#
+# MAIN
+#
+########################################################################
+if __name__ == "__main__":
+    # Manage parameters
+    parser = argparse.ArgumentParser(description='Dump run information coming from several file in run folder.')
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+    group_input = parser.add_argument_group('Inputs')
+    group_input.add_argument('-r', '--input-run-folder', required=True, help='Path to the run folder.')
+    group_output = parser.add_argument_group('Outputs')
+    group_output.add_argument('-o', '--output-info', help='Path to the outputted info file (format: JSON).')
+    args = parser.parse_args()
+
+    # Logger
+    logging.basicConfig(format='%(asctime)s - %(name)s [%(levelname)s] %(message)s')
+    log = logging.getLogger(os.path.basename(__file__))
+    log.setLevel(logging.INFO)
+    log.info("Command: " + " ".join(sys.argv))
+
+    # Process
+    with open(args.output_info, "w") as FH_out:
+        json.dump(getRunFolderInfo(args.input_run_folder), FH_out, cls=DateTimeEncoder)
+    log.info("END of process.")
