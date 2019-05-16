022c782b621456b6a0cbfa08075388ee32b2cac5
fescudie <escudie.frederic@wanadoo.fr>
2019-05-16 13:40:36 +0200

    Add bin/filterAnnotVCF.py

---
+++ bin/filterAnnotVCF.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2019 IUCT-O
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = 'Frederic Escudie'
+__copyright__ = 'Copyright (C) 2019 IUCT-O'
+__license__ = 'GNU General Public License'
+__version__ = '1.0.0'
+__email__ = 'escudie.frederic@iuct-oncopole.fr'
+__status__ = 'prod'
+
+import os
+import sys
+import json
+import logging
+import argparse
+from copy import deepcopy
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+LIB_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "lib"))
+sys.path.append(LIB_DIR)
+
+from anacore.filters import filtersFromDict
+from anacore.annotVcf import AnnotVCFIO
+
+
+########################################################################
+#
+# MAIN
+#
+########################################################################
+if __name__ == "__main__":
+    # Manage parameters
+    parser = argparse.ArgumentParser(description='Filters VCF variants and annotations on criteria described in JSON files.')
+    parser.add_argument('-f', '--annotation-field', default="ANN", help='Field used to store annotations. [Default: %(default)s]')
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+    group_input = parser.add_argument_group('Inputs')  # Inputs
+    group_input.add_argument('-r', '--input-filters-variants', help='The path to the filters file on variants (format: JSON).')
+    group_input.add_argument('-a', '--input-filters-annotations', help='The path to the filters file on annotations (format: JSON).')
+    group_input.add_argument('-i', '--input-variants', required=True, help='The path to the variants file (format: VCF).')
+    group_output = parser.add_argument_group('Outputs')  # Outputs
+    group_output.add_argument('-o', '--output-variants', required=True, help='The path to the outputted variants file (format: VCF).')
+    args = parser.parse_args()
+
+    # Logger
+    logging.basicConfig(format='%(asctime)s - %(name)s [%(levelname)s] %(message)s')
+    log = logging.getLogger(os.path.basename(__file__))
+    log.setLevel(logging.INFO)
+    log.info("Command: " + " ".join(sys.argv))
+
+    # Get filters
+    record_filters = None
+    if args.input_filters_variants is not None:
+        with open(args.input_filters_variants) as data_file:
+            record_filters = filtersFromDict(json.load(data_file))
+    annot_filters = None
+    if args.input_filters_annotations is not None:
+        with open(args.input_filters_annotations) as data_file:
+            annot_filters = filtersFromDict(json.load(data_file))
+    if record_filters is None and annot_filters is None:
+        parser.error('"--input-filters-annotations" and/or "--input-filters-annotations" must be specified.')
+
+    # Process
+    with AnnotVCFIO(args.output_variants, mode="w", annot_field=args.annotation_field) as FH_out:
+        with AnnotVCFIO(args.input_variants) as FH_in:
+            # Header
+            FH_out.copyHeader(FH_in)
+            FH_out._writeHeader()
+            # Records
+            for record in FH_in:
+                if record_filters is None or record_filters.eval(record):
+                    if annot_filters is not None:
+                        if args.annotation_field in record.info and len(record.info[args.annotation_field]) != 0:
+                            kept_annot = []
+                            for annot in record.info[args.annotation_field]:
+                                upgraded_annot = deepcopy(annot)
+                                upgraded_annot["variant"] = record
+                                if annot_filters.eval(upgraded_annot):
+                                    kept_annot.append(annot)
+                            record.info[args.annotation_field] = kept_annot
+                    FH_out.write(record)
+    log.info("End of job")
---
+++ bin/test/test_filterAnnotVCF.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2019 IUCT-O
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = 'Frederic Escudie'
+__copyright__ = 'Copyright (C) 2019 IUCT-O'
+__license__ = 'GNU General Public License'
+__version__ = '1.0.0'
+__email__ = 'escudie.frederic@iuct-oncopole.fr'
+__status__ = 'prod'
+
+import os
+import sys
+import uuid
+import tempfile
+import unittest
+import subprocess
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+APP_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
+LIB_DIR = os.path.join(APP_DIR, "lib")
+sys.path.append(LIB_DIR)
+
+from anacore.vcf import VCFRecord
+from anacore.annotVcf import AnnotVCFIO
+
+BIN_DIR = os.path.dirname(CURRENT_DIR)
+os.environ['PATH'] = BIN_DIR + os.pathsep + os.environ['PATH']
+
+
+########################################################################
+#
+# FUNCTIONS
+#
+########################################################################
+class TestFilterAnnotVCF(unittest.TestCase):
+    def setUp(self):
+        tmp_folder = tempfile.gettempdir()
+        unique_id = str(uuid.uuid1())
+
+        # Temporary files
+        self.tmp_var_filters = os.path.join(tmp_folder, unique_id + "_varFilters.json")
+        self.tmp_annot_filters = os.path.join(tmp_folder, unique_id + "_annFilters.json")
+        self.tmp_variants = os.path.join(tmp_folder, unique_id + ".vcf")
+        self.tmp_output = os.path.join(tmp_folder, unique_id + "_out.vcf")
+
+        # Command
+        self.cmd = [
+            "filterAnnotVCF.py",
+            "--input-variants", self.tmp_variants,
+            "--output-variants", self.tmp_output
+        ]
+
+        # Create filters
+        with open(self.tmp_var_filters, "w") as FH_filter:
+            FH_filter.write("""{
+    "class": "FiltersCombiner",
+    "operator": "or",
+    "filters": [
+        {
+            "class": "Filter",
+            "getter": "filter",
+            "action": "select",
+            "aggregator": "ratio:1",
+            "operator": "!=",
+            "values": "CSQ"
+        }, {
+            "class": "Filter",
+            "getter": "chrom",
+            "action": "select",
+            "aggregator": "nb:1",
+            "operator": "==",
+            "values": "artificial_chr2"
+        }
+    ]
+}""")
+        with open(self.tmp_annot_filters, "w") as FH_filter:
+            FH_filter.write("""{
+    "class": "Filter",
+    "getter": "FILTER",
+    "action": "select",
+    "aggregator": "ratio:1",
+    "operator": "==",
+    "values": "PASS"
+}""")
+
+        # Create VCF
+        with AnnotVCFIO(self.tmp_variants, "w") as FH_var:
+            FH_var.ANN_titles = ["Allele", "id", "is_filtered", "FILTER"]
+            FH_var.info = {
+                "ANN": {"type": str, "type_tag": "String", "number": None, "number_tag": ".", "description": "Consequence annotations from Ensembl VEP. Format: Allele|id|is_filtered|FILTER."},
+                "is_filtered": {"type": int, "type_tag": "Integer", "number": 1, "number_tag": "1", "description": "The expected result."}
+            }
+            FH_var._writeHeader()
+            self.variants = [
+                VCFRecord(
+                    "artificial_chr1", 10, "alt_00", "G", ["T"], None, ["PASS"],
+                    {
+                        "is_filtered": 0
+                    }
+                ),
+                VCFRecord(
+                    "artificial_chr1", 10, "alt_01", "G", ["T"], None, ["CSQ"],
+                    {
+                        "is_filtered": 1
+                    }
+                ),
+                VCFRecord(
+                    "artificial_chr2", 10, "alt_02", "G", ["T"], None, ["CSQ"],
+                    {
+                        "is_filtered": 0,  # Proctected
+                    }
+                ),
+                VCFRecord(
+                    "artificial_chr1", 10, "alt_03", "G", ["T"], None, ["PASS"],
+                    {
+                        "ANN": [{"Allele": "T", "id": "ann_00", "FILTER": "PASS", "is_filtered": 0}],
+                        "is_filtered": 0
+                    }
+                ),
+                VCFRecord(
+                    "artificial_chr1", 10, "alt_04", "G", ["T"], None, ["PASS"],
+                    {
+                        "ANN": [{"Allele": "C", "id": "ann_01", "FILTER": "ANN.COOC", "is_filtered": 1}],
+                        "is_filtered": 0
+                    }
+                ),
+                VCFRecord(
+                    "artificial_chr1", 10, "alt_05", "G", ["T"], None, ["CSQ"],
+                    {
+                        "ANN": [{"Allele": "C", "id": "ann_02", "FILTER": "ANN.COOC", "is_filtered": 1}],
+                        "is_filtered": 1
+                    }
+                ),
+                VCFRecord(
+                    "artificial_chr1", 10, "alt_06", "G", ["T"], None, ["CSQ"],
+                    {
+                        "ANN": [{"Allele": "T", "id": "ann_03", "FILTER": "PASS", "is_filtered": 0}],
+                        "is_filtered": 1
+                    }
+                ),
+                VCFRecord(
+                    "artificial_chr1", 10, "alt_07", "G", ["T"], None, ["PASS"],
+                    {
+                        "ANN": [
+                            {"Allele": "T", "id": "ann_04", "FILTER": "PASS", "is_filtered": 0},
+                            {"Allele": "C", "id": "ann_05", "FILTER": "ANN.COOC", "is_filtered": 1},
+                        ],
+                        "is_filtered": 0
+                    }
+                ),
+                VCFRecord(
+                    "artificial_chr1", 10, "alt_08", "G", ["T"], None, ["PASS"],
+                    {
+                        "ANN": [
+                            {"Allele": "T", "id": "ann_06", "FILTER": "ANN.popAF", "is_filtered": 1},
+                            {"Allele": "C", "id": "ann_07", "FILTER": "ANN.COOC&ANN.popAF", "is_filtered": 1},
+                        ],
+                        "is_filtered": 0
+                    }
+                ),
+                VCFRecord(
+                    "artificial_chr2", 10, "alt_09", "G", ["T"], None, ["CSQ"],
+                    {
+                        "ANN": [
+                            {"Allele": "T", "id": "ann_08", "FILTER": "ANN.popAF", "is_filtered": 1},
+                            {"Allele": "C", "id": "ann_09", "FILTER": "ANN.COOC&ANN.popAF", "is_filtered": 1},
+                        ],
+                        "is_filtered": 0  # Protected
+                    }
+                ),
+                VCFRecord(
+                    "artificial_chr2", 10, "alt_10", "G", ["T"], None, ["CSQ"],
+                    {
+                        "ANN": [
+                            {"Allele": "T", "id": "ann_10", "FILTER": "PASS", "is_filtered": 0},
+                            {"Allele": "C", "id": "ann_11", "FILTER": "ANN.COOC&ANN.popAF", "is_filtered": 1},
+                        ],
+                        "is_filtered": 0  # Protected
+                    }
+                )
+            ]
+            for idx, curr_var in enumerate(self.variants):
+                FH_var.write(curr_var)
+
+    def tearDown(self):
+        # Clean temporary files
+        for curr_file in [self.tmp_var_filters, self.tmp_annot_filters, self.tmp_variants, self.tmp_output]:
+            if os.path.exists(curr_file):
+                os.remove(curr_file)
+
+    def testMissingFilterException(self):
+        with self.assertRaises(subprocess.CalledProcessError) as context:
+            subprocess.check_call(self.cmd, stderr=subprocess.DEVNULL)
+
+    def testResultsFilterVar(self):
+        cmd = self.cmd + ["--input-filters-variants", self.tmp_var_filters]
+
+        # Execute command
+        subprocess.check_call(cmd, stderr=subprocess.DEVNULL)
+
+        # Validate results
+        expected = list()
+        for record in self.variants:
+            if record.info["is_filtered"] == 0:
+                expected.append(record.id)
+                if "ANN" in record.info:
+                    for curr_ann in record.info["ANN"]:
+                        expected.append(curr_ann["id"])
+        observed = list()
+        with AnnotVCFIO(self.tmp_output) as FH_results:
+            for record in FH_results:
+                observed.append(record.id)
+                if "ANN" in record.info:
+                    for curr_ann in record.info["ANN"]:
+                        observed.append(curr_ann["id"])
+        self.assertEqual(expected, observed)
+
+    def testResultsFilterAnn(self):
+        cmd = self.cmd + ["--input-filters-annotations", self.tmp_annot_filters]
+
+        # Execute command
+        subprocess.check_call(cmd, stderr=subprocess.DEVNULL)
+
+        # Validate results
+        expected = list()
+        for record in self.variants:
+            expected.append(record.id)
+            if "ANN" in record.info:
+                for curr_ann in record.info["ANN"]:
+                    if curr_ann["is_filtered"] == 0:
+                        expected.append(curr_ann["id"])
+        observed = list()
+        with AnnotVCFIO(self.tmp_output) as FH_results:
+            for record in FH_results:
+                observed.append(record.id)
+                if "ANN" in record.info:
+                    for curr_ann in record.info["ANN"]:
+                        observed.append(curr_ann["id"])
+
+        self.assertEqual(expected, observed)
+
+    def testResultsFilterAnnAndVar(self):
+        cmd = self.cmd + ["--input-filters-variants", self.tmp_var_filters, "--input-filters-annotations", self.tmp_annot_filters]
+
+        # Execute command
+        subprocess.check_call(cmd, stderr=subprocess.DEVNULL)
+
+        # Validate results
+        expected = list()
+        for record in self.variants:
+            if record.info["is_filtered"] == 0:
+                expected.append(record.id)
+                if "ANN" in record.info:
+                    for curr_ann in record.info["ANN"]:
+                        if curr_ann["is_filtered"] == 0:
+                            expected.append(curr_ann["id"])
+        observed = list()
+        with AnnotVCFIO(self.tmp_output) as FH_results:
+            for record in FH_results:
+                observed.append(record.id)
+                if "ANN" in record.info:
+                    for curr_ann in record.info["ANN"]:
+                        observed.append(curr_ann["id"])
+        self.assertEqual(expected, observed)
+
+
+########################################################################
+#
+# MAIN
+#
+########################################################################
+if __name__ == "__main__":
+    unittest.main()
