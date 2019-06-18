442ea126bb4463c2c41f3b49f7bb2ee700a0de48
fescudie <escudie.frederic@wanadoo.fr>
2019-06-18 18:06:36 +0200

    Fix execution error.

--- bin/illuCountBarcodes.py
+++ bin/illuCountBarcodes.py
@@ -19,7 +19,7 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2018 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -43,11 +43,19 @@ from anacore.illumina import getInfFromSeqDesc
 #
 ########################################################################
 def getCountByBarcode(in_seq):
+    """
+    Return the number of reads by barcode in the fastq file.
+
+    :param in_seq: The path to the sequence file (format: fastq).
+    :type in_seq: int
+    :return: The number of reads by barcode.
+    :rtype: dict
+    """
     count_by_barcode = dict()
     for curr_seq in in_seq:
         with FastqIO(curr_seq) as FH_in:
             for record in FH_in:
-                barcode = getInfFromSeqDesc(record.desc)["barcode"]
+                barcode = getInfFromSeqDesc(record.description)["barcode"]
                 if barcode not in count_by_barcode:
                     count_by_barcode[barcode] = 1
                 else:
@@ -55,29 +63,59 @@ def getCountByBarcode(in_seq):
     return count_by_barcode
 
 def getTopN(count_by_key, nb_top, is_desc=True):
+    """
+    Return the number of reads by barcode for the N most or least abundant.
+
+    :param count_by_key: The number of reads by barcode.
+    :type count_by_key: dict
+    :param nb_top: The number of selected barcodes.
+    :type nb_top: int
+    :param is_desc: If true the most abudant are selected. otherwise, the least abundant are selected.
+    :type is_desc: bool
+    :return: The number of reads by barcode for the N most or least abundant.
+    :rtype: dict
+    """
     restricted_count_by_key = dict()
     if len(count_by_key) <= nb_top:
         restricted_count_by_key = copy.deepcopy(count_by_key)
     else:
         restricted_count_by_key = dict()
-        for key in sorted(count_by_key, key=count_by_key.get, reverse=is_desc)[nb_top]:
+        for key in sorted(count_by_key, key=count_by_key.get, reverse=is_desc)[:nb_top]:
             restricted_count_by_key[key] = count_by_key[key]
     return restricted_count_by_key
 
 def writeJSON(data, out_path):
+    """
+    Write the results in a JSON file.
+
+    :param data: Analysis results.
+    :type data: dict
+    :param out_path: Path to the output file.
+    :type out_path: str
+    """
     with open(out_path, "w") as FH_out:
         FH_out.write(
             json.dumps(data, default=lambda o: o.__dict__, sort_keys=True)
         )
 
 def writeSV(data, out_path, separator="\t"):
+    """
+    Write the results in a JSON file.
+
+    :param data: Analysis results.
+    :type data: dict
+    :param out_path: Path to the output file.
+    :type out_path: str
+    :param separator: The field separator used in the output file.
+    :type separator: str
+    """
     with open(out_path, "w") as FH_out:
         FH_out.write(
             separator.join(["#Barcode", "Nb_barcodes", "Count\n"])
         )
-        for key in sorted(data["count_by_key"], key=data["count_by_key"].get, reverse=True):
+        for key in sorted(data["count_by_barcode"], key=data["count_by_barcode"].get, reverse=True):
             FH_out.write(
-                separator.join([key, "1", data["count_by_key"][key] + "\n"])
+                separator.join([key, "1", str(data["count_by_barcode"][key]) + "\n"])
             )
         # Add count for others barcodes
         if data["limit_barcodes"] is not None and data["limit_barcodes"] < data["nb_barcodes"]:
