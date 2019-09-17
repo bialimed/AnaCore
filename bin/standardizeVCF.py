b43c5a895cbca84f024113410cc19cded56ddd72
fescudie <escudie.frederic@iuct-oncopole.fr>
2019-09-17 13:50:27 +0200

    Merge bin/standardizeVariants.py and bin/stdVCFToSpecVCF.py in one script and add annotations management.

---
+++ bin/standardizeVCF.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2019 IUCT-O
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = 'Frederic Escudie'
+__copyright__ = 'Copyright (C) 2019 IUCT-O'
+__license__ = 'GNU General Public License'
+__version__ = '1.0.0'
+__email__ = 'escudie.frederic@iuct-oncopole.fr'
+__status__ = 'prod'
+
+import os
+import sys
+import logging
+import argparse
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+LIB_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "lib"))
+sys.path.append(LIB_DIR)
+
+from anacore.vcf import VCFIO, VCFRecord, getAlleleRecord, HeaderInfoAttr
+from anacore.annotVcf import AnnotVCFIO
+from anacore.sequenceIO import FastaIO, IdxFastaIO
+
+
+########################################################################
+#
+# FUNCTIONS
+#
+########################################################################
+def stdizeVCF(FH_ref, FH_in, FH_out, trace_unstandard=False, log=None):
+    """
+    Split alternatives alleles in multi-lines, removes unecessary reference and alternative nucleotids, move indel to most upstream position and update alt allele in annotations.
+
+    :param FH_ref: File handle to the reference file (format: fasta with faidx).
+    :type FH_ref: anacore.sequenceIO.IdxFastaIO
+    :param FH_in: File handle to the variants file (format: VCF).
+    :type FH_in: anacore.vcf.VCFIO
+    :param FH_out: File handle to the standardized variants file (format: VCF).
+    :type FH_out: anacore.vcf.VCFIO
+    :param trace_unstandard: True if you want to keep the trace of the variant before standardization in INFO.
+    :type trace_unstandard: bool
+    :param log: Logger used.
+    :type log: logging.Logger
+    """
+    nb_annot = {"exact": 0, "collocated": 0}
+    is_annotated = issubclass(FH_out.__class__, AnnotVCFIO)
+    # Header
+    FH_out.copyHeader(FH_in)
+    if trace_unstandard:
+        FH_out.info["UNSTD"] = HeaderInfoAttr("UNSTD", type="String", number="1", description="The variant id (chromosome:position=reference/alternative) before standardization.")
+    FH_out.writeHeader()
+    # Records
+    for record in FH_in:
+        chrom = FH_ref.get(record.chrom).string
+        collocated_records = []
+        for alt_idx, alt in enumerate(record.alt):
+            alt_record = getAlleleRecord(FH_in, record, alt_idx)
+            if trace_unstandard:
+                alt_record.info["UNSTD"] = alt_record.getName()
+            # Previous
+            unstd = {"chrom": alt_record.chrom, "pos": alt_record.pos, "ref": alt_record.ref, "alt": alt_record.alt[0]}
+            # To minimal string
+            alt_record.standardizeSingleAllele()
+            # Move to upstream
+            alt_record = alt_record.getMostUpstream(chrom)
+            # Add previous nt
+            if alt_record.isIndel():
+                prev_nt = chrom[alt_record.pos - 2]
+                if alt_record.ref == VCFRecord.getEmptyAlleleMarker():  # Insertion
+                    alt_record.ref = prev_nt
+                    alt_record.alt[0] = prev_nt + alt_record.alt[0]
+                    alt_record.pos -= 1
+                elif alt_record.alt[0] == VCFRecord.getEmptyAlleleMarker():  # Deletion
+                    alt_record.ref = prev_nt + alt_record.ref
+                    alt_record.alt[0] = prev_nt
+                    alt_record.pos -= 1
+            # Update annotations
+            if is_annotated and FH_in.annot_field in alt_record.info:
+                cleaned_annot = []
+                for idx_ann, annot in enumerate(alt_record.info[FH_in.annot_field]):
+                    if unstd["alt"] == annot["Allele"]:
+                        nb_annot["exact"] += 1
+                        annot["Allele"] = alt_record.alt[0]
+                        cleaned_annot.append(annot)
+                    else:
+                        nb_annot["collocated"] += 1
+                alt_record.info[FH_in.annot_field] = cleaned_annot
+            collocated_records.append(alt_record)
+        if len(collocated_records) == 1:
+            FH_out.write(collocated_records[0])
+        else:
+            for alt_record in sorted(collocated_records, key=lambda elt: (elt.refStart(), elt.refEnd())):  # Sorted splitted alleleles
+                FH_out.write(alt_record)
+    if log is not None and nb_annot["collocated"] != 0:
+        log.warning("{}/{} annotations have been deleted because they concern collocated variant.".format(nb_annot["collocated"], nb_annot["exact"] + nb_annot["collocated"]))
+
+
+########################################################################
+#
+# MAIN
+#
+########################################################################
+if __name__ == "__main__":
+    # Manage parameters
+    parser = argparse.ArgumentParser(description='Splits alternatives alleles in multi-lines and removes unecessary reference and alternative nucleotids, move indel to most upstream position and update alt allele in annotations.')
+    parser.add_argument('-a', '--annotations-field', help='Field used to store annotations if the variants file is annotated.')
+    parser.add_argument('-t', '--trace-unstandard', action='store_true', help='Use this option to add "UNSTD" tag in record INFO. This tag contains the trace of the variant before standardization: chromosome:position=reference/alternative.')
+    parser.add_argument('-v', '--version', action='version', version=__version__)
+    group_input = parser.add_argument_group('Inputs')  # Inputs
+    group_input.add_argument('-i', '--input-variants', required=True, help='The path to the variants file (format: VCF).')
+    group_input.add_argument('-r', '--input-reference', required=True, help='The path to the reference used in variant calling to produced the input VCF (format: fasta with faidx).')
+    group_output = parser.add_argument_group('Outputs')  # Outputs
+    group_output.add_argument('-o', '--output-variants', required=True, help='The path to the output file (format: VCF).')
+    args = parser.parse_args()
+
+    # Logger
+    logging.basicConfig(format='%(asctime)s -- [%(filename)s][pid:%(process)d][%(levelname)s] -- %(message)s')
+    log = logging.getLogger(os.path.basename(__file__))
+    log.setLevel(logging.INFO)
+    log.info("Command: " + " ".join(sys.argv))
+
+    # Process
+    with IdxFastaIO(args.input_reference, use_cache=True) as FH_ref:
+        if args.annotations_field is not None:
+            with AnnotVCFIO(args.output_variants, "w", annot_field=args.annotations_field) as FH_out:
+                with AnnotVCFIO(args.input_variants, annot_field=args.annotations_field) as FH_in:
+                    stdizeVCF(FH_ref, FH_in, FH_out, args.trace_unstandard, log)
+        else:
+            with VCFIO(args.output_variants, "w") as FH_out:
+                with VCFIO(args.input_variants) as FH_in:
+                    stdizeVCF(FH_ref, FH_in, FH_out, args.trace_unstandard, log)
+
+    log.info("End of job")
---
+++ bin/test/test_standardizeVCF.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2019 IUCT-O
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+__author__ = 'Frederic Escudie'
+__copyright__ = 'Copyright (C) 2019 IUCT-O'
+__license__ = 'GNU General Public License'
+__version__ = '1.0.0'
+__email__ = 'escudie.frederic@iuct-oncopole.fr'
+__status__ = 'prod'
+
+import os
+import sys
+import uuid
+import tempfile
+import unittest
+import subprocess
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+APP_DIR = os.path.dirname(os.path.dirname(CURRENT_DIR))
+LIB_DIR = os.path.join(APP_DIR, "lib")
+sys.path.append(LIB_DIR)
+
+from anacore.bed import BEDIO, BEDRecord
+from anacore.vcf import VCFIO, VCFRecord, HeaderInfoAttr
+from anacore.sequenceIO import FastaIO, Sequence
+
+BIN_DIR = os.path.dirname(CURRENT_DIR)
+os.environ['PATH'] = BIN_DIR + os.pathsep + os.environ['PATH']
+
+
+########################################################################
+#
+# FUNCTIONS
+#
+########################################################################
+class StandardizeVCF(unittest.TestCase):
+    def setUp(self):
+        tmp_folder = tempfile.gettempdir()
+        unique_id = str(uuid.uuid1())
+
+        # Temporary files
+        self.tmp_sequences = os.path.join(tmp_folder, unique_id + ".fasta")
+        self.tmp_faidx = os.path.join(tmp_folder, unique_id + ".fasta.fai")
+        self.tmp_variants = os.path.join(tmp_folder, unique_id + ".vcf")
+        self.tmp_output = os.path.join(tmp_folder, unique_id + "_out.vcf")
+
+        # Exec command
+        self.cmd = [
+            "standardizeVCF.py",
+            "--trace-unstandard",
+            "--input-reference", self.tmp_sequences,
+            "--input-variants", self.tmp_variants,
+            "--output-variants", self.tmp_output
+        ]
+
+        # Create fasta
+        with FastaIO(self.tmp_sequences, "w") as FH_seq:
+            # Repeats:                                       ****....            ...***
+            # Region:                                 |----|        |------------|         |------|
+            FH_seq.write(Sequence("artificial_chr1", "CTCAGTCATGTATGTATGTGCTCACAAAGTAGTAGATCATGGCAC"))
+            #                                         123456789| | | | | | | | | | | | | | | | | |
+            #                                                  10| 14| 18| 22| 26| 30| 34| 38| 42|
+            #                                                    12  16  20  24  28  32  36  40  44
+            FH_seq.write(Sequence("artificial_chr2", "CGATNNNCGAT"))
+            #                                         123456789|
+            #                                                  10
+
+        # Create faidx
+        with open(self.tmp_faidx, "w") as FH_fai:
+            FH_fai.write("""artificial_chr1	45	17	45	46
+artificial_chr2	11	80	11	12""")
+
+        # Create VCF
+        with VCFIO(self.tmp_variants, "w") as FH_var:
+            FH_var.info = {
+                "expected": HeaderInfoAttr("expected", "Standardized version of {chrom}:{pos}={ref}/{alt}.", type="String", number="."),
+                "ANN": HeaderInfoAttr("ANN", "Annotation of variants Format: Allele|Annotation_id|Alt_allele_idx", type="String", number="."),
+                "expectedANN": HeaderInfoAttr("expectedANN", "Standardized version of annotations Format: Allele|Annotation_id|Alt_allele_idx", type="String", number=".")
+            }
+            FH_var.writeHeader()
+            self.variants = [
+                # Substit single nt
+                VCFRecord("artificial_chr1", 14, "sub_01", "G", ["T"], None, None, {
+                    "expected": ["artificial_chr1:14=G/T"],
+                    "ANN": ["T|ann_1|0", "T|ann_2|0", "A|ann_3|"],
+                    "expectedANN": ["T|ann_1|0", "T|ann_2|0"]
+                }),
+                VCFRecord("artificial_chr1", 19, "sub_02", "T", ["A", "C"], None, None, {
+                    "expected": ["artificial_chr1:19=T/A", "artificial_chr1:19=T/C"],
+                    "ANN": ["A|ann_1|0", "A|ann_2|0", "T|ann_3|"],
+                    "expectedANN": ["A|ann_1|0", "A|ann_2|0"]
+                }),
+                # Substit multi nt
+                VCFRecord("artificial_chr1", 7, "sub_03", "CATGTATG", ["GTACCCGC"], None, None, {
+                    "expected": ["artificial_chr1:7=CATGTATG/GTACCCGC"],
+                    "ANN": ["GTACCCGC|ann_1|0", "GTACCCGC|ann_2|0", "GTGT|ann_3|"],
+                    "expectedANN": ["GTACCCGC|ann_1|0", "GTACCCGC|ann_2|0"]
+                }),
+                VCFRecord("artificial_chr1", 11, "sub_04", "TATGTATG", ["GTACCCGC", "GTACCCAA"], None, None, {
+                    "expected": ["artificial_chr1:11=TATGTATG/GTACCCGC", "artificial_chr1:11=TATGTATG/GTACCCAA"],
+                    "ANN": ["GTACCCGC|ann_1|0", "GTACCCGC|ann_2|0", "GTACCCAA|ann_3|1"],
+                    "expectedANN": ["GTACCCGC|ann_1|0", "GTACCCGC|ann_2|0", "GTACCCAA|ann_3|1"]
+                }),
+                # Insertion single nt
+                VCFRecord("artificial_chr1", 14, "ins_01", "G", ["GA"], None, None, {
+                    "expected": ["artificial_chr1:14=G/GA"],
+                    "ANN": ["GA|ann_1|0", "GA|ann_2|0", "GT|ann_3|"],
+                    "expectedANN": ["GA|ann_1|0", "GA|ann_2|0"]
+                }),
+                VCFRecord("artificial_chr1", 20, "ins_02", "-", ["A"], None, None, {
+                    "expected": ["artificial_chr1:19=T/TA"],
+                    "ANN": ["A|ann_1|0", "A|ann_2|0", "T|ann_3|"],
+                    "expectedANN": ["TA|ann_1|0", "TA|ann_2|0"]
+                }),
+                VCFRecord("artificial_chr1", 14, "ins_03", "G", ["GA", "GC"], None, None, {
+                    "expected": ["artificial_chr1:14=G/GA", "artificial_chr1:14=G/GC"],
+                    "ANN": ["GA|ann_1|0", "GA|ann_2|0", "GC|ann_3|1", "GT|ann_4|"],
+                    "expectedANN": ["GA|ann_1|0", "GA|ann_2|0", "GC|ann_3|1"]
+                }),
+                VCFRecord("artificial_chr1", 20, "ins_04", "-", ["A", "C"], None, None, {
+                    "expected": ["artificial_chr1:19=T/TA", "artificial_chr1:19=T/TC"],
+                    "ANN": ["A|ann_1|0", "A|ann_2|0", "C|ann_3|1", "T|ann_4|"],
+                    "expectedANN": ["TA|ann_1|0", "TA|ann_2|0", "TC|ann_3|1"]
+                }),
+                # Insertion multi nt
+                VCFRecord("artificial_chr1", 14, "ins_05", "G", ["GATGC"], None, None, {
+                    "expected": ["artificial_chr1:14=G/GATGC"],
+                    "ANN": ["GATGC|ann_1|0", "GATGC|ann_2|0", "GAAAC|ann_3|"],
+                    "expectedANN": ["GATGC|ann_1|0", "GATGC|ann_2|0"]
+                }),
+                VCFRecord("artificial_chr1", 20, "ins_06", "-", ["AAATC"], None, None, {
+                    "expected": ["artificial_chr1:19=T/TAAATC"],
+                    "ANN": ["AAATC|ann_1|0", "AAATC|ann_2|0", "GAAAC|ann_3|"],
+                    "expectedANN": ["TAAATC|ann_1|0", "TAAATC|ann_2|0"]
+                }),
+                # Movable insertion multi nt
+                VCFRecord("artificial_chr1", 14, "ins_07", "G", ["GTG"], None, None, {
+                    "expected": ["artificial_chr1:12=A/ATG"],
+                    "ANN": ["GTG|ann_1|0", "GTG|ann_2|0", "GAAAC|ann_3|"],
+                    "expectedANN": ["ATG|ann_1|0", "ATG|ann_2|0"]
+                }),
+                VCFRecord("artificial_chr1", 27, "ins_08", "A", ["AAAA"], None, None, {
+                    "expected": ["artificial_chr1:25=C/CAAA"],
+                    "ANN": ["AAAA|ann_1|0", "AAAA|ann_2|0", "CAAA|ann_3|"],
+                    "expectedANN": ["CAAA|ann_1|0", "CAAA|ann_2|0"]
+                }),
+                # Deletion single nt
+                VCFRecord("artificial_chr1", 14, "del_01", "G", [""], None, None, {
+                    "expected": ["artificial_chr1:13=TG/T"],
+                    "ANN": ["-|ann_1|0", "-|ann_2|0", "T|ann_3|"],
+                    "expectedANN": ["T|ann_1|0", "T|ann_2|0"]
+                }),
+                VCFRecord("artificial_chr1", 14, "del_02", "G", ["-"], None, None, {
+                    "expected": ["artificial_chr1:13=TG/T"],
+                    "ANN": ["-|ann_1|0", "-|ann_2|0", "T|ann_3|"],
+                    "expectedANN": ["T|ann_1|0", "T|ann_2|0"]
+                }),
+                VCFRecord("artificial_chr1", 13, "del_03", "TG", ["T"], None, None, {
+                    "expected": ["artificial_chr1:13=TG/T"],
+                    "ANN": ["T|ann_1|0", "T|ann_2|0", "A|ann_3|"],
+                    "expectedANN": ["T|ann_1|0", "T|ann_2|0"]
+                }),
+                VCFRecord("artificial_chr1", 13, "del_04", "TG", ["T", "-"], None, None, {
+                    "expected": ["artificial_chr1:13=TG/T", "artificial_chr1:12=ATG/A"],
+                    "ANN": ["T|ann_1|0", "T|ann_2|0", "-|ann_3|1"],
+                    "expectedANN": ["T|ann_1|0", "T|ann_2|0", "A|ann_3|1"]
+                }),
+                # Movable deletion multi nt
+                VCFRecord("artificial_chr1", 11, "del_05", "TATG", ["T", "TA", "-"], None, None, {
+                    "expected": ["artificial_chr1:11=TATG/T", "artificial_chr1:12=ATG/A", "artificial_chr1:7=CATGT/C"],
+                    "ANN": ["T|ann_1|0", "T|ann_2|0", "TA|ann_3|1", "-|ann_4|2"],
+                    "expectedANN": ["T|ann_1|0", "T|ann_2|0", "A|ann_3|1", "C|ann_4|2"]
+                }),
+            ]
+            for idx, curr_var in enumerate(self.variants):
+                FH_var.write(curr_var)
+
+    def tearDown(self):
+        # Clean temporary files
+        for curr_file in [self.tmp_sequences, self.tmp_faidx, self.tmp_variants, self.tmp_output]:
+            if os.path.exists(curr_file):
+                os.remove(curr_file)
+
+    def testVCFIO(self):
+        # Execute command
+        subprocess.check_call(self.cmd, stderr=subprocess.DEVNULL)
+
+        # Validate results
+        expected = {}
+        for record in self.variants:
+            for idx, alt in enumerate(record.alt):
+                id = "{} {}:{}={}/{}".format(record.id, record.chrom, record.pos, record.ref, alt)
+                expected[id] = record.info["expected"][idx]
+        observed = {}
+        with VCFIO(self.tmp_output) as FH_results:
+            for record in FH_results:
+                observed[record.id + " " + record.info["UNSTD"]] = record.getName()
+        self.assertEqual(
+            expected,
+            observed
+        )
+
+    def testAnnotVCFIO(self):
+        # Execute command
+        subprocess.check_call(self.cmd + ["--annotations-field", "ANN"], stderr=subprocess.DEVNULL)
+
+        # Validate results
+        expected = {}
+        for record in self.variants:
+            for idx, alt in enumerate(record.alt):
+                id = "{} {}:{}={}/{}".format(record.id, record.chrom, record.pos, record.ref, alt)
+                expected[id] = record.info["expected"][idx]
+        observed = {}
+        with VCFIO(self.tmp_output) as FH_results:
+            for record in FH_results:
+                observed[record.id + " " + record.info["UNSTD"]] = record.getName()
+        self.assertEqual(
+            expected,
+            observed
+        )
+
+        # Validate annotations
+        expected = {}
+        for record in self.variants:
+            for idx, alt in enumerate(record.alt):
+                id = "{} {}:{}={}/{}".format(record.id, record.chrom, record.pos, record.ref, alt)
+                expected[id] = sorted([ann for ann in record.info["expectedANN"] if ann.split("|")[2] == str(idx)])
+                print(id, expected[id])
+        observed = {}
+        with VCFIO(self.tmp_output) as FH_results:
+            for record in FH_results:
+                id = record.id + " " + record.info["UNSTD"]
+                observed[id] = []
+                if "ANN" in record.info:
+                    observed[id] = sorted([ann for ann in record.info["ANN"]])
+        self.assertEqual(
+            expected,
+            observed
+        )
+
+
+########################################################################
+#
+# MAIN
+#
+########################################################################
+if __name__ == "__main__":
+    unittest.main()
